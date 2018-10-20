2f5d54debac0c2da424a78791f7ae6a45726dfa4
Damião <joker.dss@gmail.com>
2018-10-20 23:17:40 +0100

    no message

--- RMI/src/RmiInterface.java
+++ RMI/src/RmiInterface.java
@@ -4,4 +4,10 @@ import java.rmi.RemoteException;
 public interface RmiInterface extends Remote {
     public String verificaLogin(String username, String password) throws RemoteException;
     public String verificaSignUp(String username, String password) throws RemoteException;
+    public String inserirMusica(String nome, String artista, String album, String duracao, String username) throws RemoteException;
+    public String inserirArtista(String artista, String album, String username) throws RemoteException;
+    public String inserirAlbum(String album, String artista, String musicas, String username) throws RemoteException;
+    public String listar(String categoria, String username) throws RemoteException;
+    public String alterar(String categoria, String index, String campo, String novo, String username) throws RemoteException;
+    public String remover(String categoria, String index, String username) throws RemoteException;
 }
